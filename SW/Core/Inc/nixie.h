84cb2709584f05c33230746805e889321a777c8e
fabi1609 <mochfabian@googlemail.com>
2019-11-12 15:29:57 +0100

    Set target voltage

--- SW/Core/Inc/nixie.h
+++ SW/Core/Inc/nixie.h
@@ -11,7 +11,7 @@
 #include "stm32g0xx_hal.h"
 #include "stm32g0xx_ll_system.h"
 
-#define DAC_TARGET 500 //TODO Anpassen
+#define DAC_TARGET 2940 //2940 approx. 160V    4095 -> 223V
 
 void nixie_init();
 void boost_op();
